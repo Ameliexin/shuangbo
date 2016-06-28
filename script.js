266a48bbb37a0b92b9f53a7c88923ade79b81a1e
谢欣 <289396546@qq.com>
2016-06-28 16:16:16 +0800

    新闻轮播

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 /**
  * Created by xiexin on 16/6/26.
  */
+var con = document.getElementById('con');
+var con1 = document.getElementById('con1');
+var con2 = document.getElementById('con2');
+con2.innerHTML = con1.innerHTML;
+function scrollUp() {
+    if(con.scrollTop >= con1.offsetHeight){
+        con.scrollTop = 0;
+    }
+    else {
+        con.scrollTop++;
+    }
+}
+var time = 50;
+var myScroll = setInterval('scrollUp()',time);
